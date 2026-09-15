df667ab59183a3c8a4b9cd5ec9a3316b8614a2e7
kill <kill@kill.kill>
2026-09-15 19:50:20 +0300

    Initial commit

---
+++ guard.py
@@ -0,0 +1,29 @@
+injections = [
+    "ignore previous instructions",
+    "you are now",
+    "forget your training",
+    "jailbreak",
+    "do anything now",
+    "DAN",
+    "pretend you are",
+    "act as if",
+    "your new instructions",
+    "disregard your",
+    "override your",
+    "bypass your",
+    "you have no restrictions",
+    "ignore all previous",
+    "new persona",
+]
+def find(prompt):
+    found = []
+    for injection in injections:
+        if prompt == injection:
+           found.append(prompt)
+    return found
+run = find(input(": "))      
+if run:
+    for r in run:
+        print("Dangerous prompt!!")
+else:
+    print("Safe prompt")
